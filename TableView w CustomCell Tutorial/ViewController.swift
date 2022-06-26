521b23603229cf9c4d967f3a7e76dc1498635325
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-06-26 22:59:13 +0300

    Initial Commit

---
+++ TableView w CustomCell Tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView w CustomCell Tutorial
+//
+//  Created by Ömer Faruk Kılıçaslan on 26.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
